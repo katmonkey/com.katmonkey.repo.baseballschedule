3fa5a56a3bdbf7924c2919bb2e82a2a634c3e8eb
tcasper <tkatmonkey@gmail.com>
2013-05-08 16:45:34 -0400

    Started adding implementation for ArrayAdapter.:

--- src/com/katmonkey/baseballschedule/MainActivity.java
+++ src/com/katmonkey/baseballschedule/MainActivity.java
@@ -1,22 +1,38 @@
 package com.katmonkey.baseballschedule;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<ArrayAdapter>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        String[] schedule = {"This", "is", "a", "ridiculous", "list"};
+        ArrayAdapter adapter = new ArrayAdapter(this, R.id.scheduleItem, schedule);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public Loader<ArrayAdapter> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return new Loader<ArrayAdapter>(this);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<ArrayAdapter> loader, ArrayAdapter data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<ArrayAdapter> loader) {
+		// TODO Auto-generated method stub
+		
+	}    
 }
